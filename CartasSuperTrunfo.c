a062c9f8adfdd971aeb47ea582675c219dae3412
guilhermerodriguescr5-afk <guilhermerodriguescr5@gmail.com>
2026-03-31 02:07:50 +0000

    segunda feira, 17 de junho de 2024, 16:00:00 segunda parte

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,17 +6,52 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidade // 
-int carta1;
-char CódigoCarta[2];
-char NomeCidade[50];
-int População
-float Área
-
+int carta;
+char estado[50];
+char código[50];
+char Cidade[50];
+int População;
+float Área;
+float PIB;
+int  Turísmo;
 
 
   // Área para entrada de dados
+printf("Digite seu estado: ");
+scanf("%s", &estado);
 
-  // Área para exibição dos dados da cidade
+printf("Digite o código do país: ");
+scanf("%s", &código);
 
+printf("Digite o nome da cidade: ");
+scanf("%s", &Cidade);
+
+printf("Digite a população: ");
+scanf("%d", &População);
+
+printf("Digite a área: ");
+scanf("%f", &Área);
+
+printf("Digite o PIB: ");
+scanf("%f", &PIB);
+
+printf("Digite o número de Pontos Turísticos: ");
+scanf("%d", &Turísmo);
+
+
+  // Área para exibição dos dados da cidade//
+  printf("estado: %s\n", estado);
+
+  printf("código: %s\n", código);
+
+  printf("cidade: %s\n", Cidade);
+
+  printf("população: %d\n", População);
+
+  printf("área: %.2f\n", Área);
+  
+  printf("PIB: %.2f\n", PIB);
+  
+  printf("pontos turísticos: %d\n", Turísmo);
 return 0;
 } 
